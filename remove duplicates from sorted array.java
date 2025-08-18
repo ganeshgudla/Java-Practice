16f007d28072f3ece7fb63db4203c1471fef9821
Ganesh Gudla <ganeshgudla9347dl@gmail.com>
2025-08-18 23:06:14 +0530

    Create remove duplicates from sorted array.java

---
+++ remove duplicates from sorted array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int uniq=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[uniq]!=nums[i]){
+                nums[++uniq]=nums[i];
+                
+            }
+        }
+        return uniq+1;
+        
+    }
+}
